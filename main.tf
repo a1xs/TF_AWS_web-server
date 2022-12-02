06577a917d1bd29d5c074234b70d3642f72f58b1
work <alexey.zyablikov@codeinside.ru>
2022-12-02 15:01:10 +0200

    static_ip fix

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web_server" {
 }
 
 locals {
-    name = ["http", "https"]
+#     name = ["http", "https"]
     ports = [80, 443]
 }
 
@@ -54,52 +54,54 @@ resource "aws_security_group" "web_server_g" {
 #      cidr_blocks      = ["0.0.0.0/0"]
 #      ipv6_cidr_blocks = ["::/0"]
 #    }
-#  }
+#}
 
-  ingress {
-    description      = "http"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    ingress {
+        description      = "http"
+        from_port        = 80
+        to_port          = 80
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        description      = "http"
+        from_port        = 443
+        to_port          = 443
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        description      = "ssh"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "Web Server Security Group"
+    }
 }
 
-  ingress {
-    description      = "http"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "ssh"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "Web Server Security Group"
-  }
-}
-
-output "webserver_aws_instance" {
+output "web_server_instance_id" {
+    description = "Instance ID"
     value = aws_instance.web_server[0].id
 }
 
-output "instance_public_ip" {
+output "web_server_instance_pub_ip" {
     description = "Public IP of EC2 instance"
     value       = aws_instance.web_server[0].public_ip
 }
